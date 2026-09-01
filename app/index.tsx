8fe5076c660cf90b572963fc562133388b31078b
Rana M Junaid Afzal <j.4junaidrana855@gamil.com>
2026-09-01 18:32:11 +0500

    Initial project setup done

---
+++ app/index.tsx
@@ -0,0 +1,6 @@
+import { useState } from "react";
+
+export default function Index() {
+  const [loading, setLoading] = useState(true);
+  const [showVideo, setShowVideo] = useState(false);
+}
---
+++ components/VideoSplash.tsx
@@ -0,0 +1,48 @@
+import { useVideoPlayer, VideoView } from "expo-video";
+import React, { useEffect } from "react";
+import { StyleSheet, View } from "react-native";
+
+type VideoSplashProp = {
+  onFinish: () => void;
+};
+
+const VideoSplash = ({ onFinish }: VideoSplashProp) => {
+  const player = useVideoPlayer(
+    require("../assets/video/tanposhsplash.mp4"),
+    (player) => {
+      player.loop = false;
+      player.play();
+    },
+  );
+  // this hook hold our videosplash when to start and End //
+  useEffect(() => {
+    const mood = player.addListener("playToEnd", () => {
+      onFinish();
+    });
+    return () => mood.remove();
+  }, [player, onFinish]);
+
+  return (
+    <View style={styles.container}>
+      <VideoView
+        player={player}
+        style={styles.video}
+        contentFit="cover"
+        nativeControls={false}
+      />
+    </View>
+  );
+};
+
+export default VideoSplash;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#0B1F3A",
+  },
+  video: {
+    width: "100%",
+    height: "100%",
+  },
+});
